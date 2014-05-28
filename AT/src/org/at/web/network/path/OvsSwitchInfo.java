32ac5841d94d363fd4738784a22bf8fe9692ea8a
Pasquale Di Rienzo <phate867@gmail.com>
2014-05-28 10:23:57 +0200

    fsfsd

--- AT/src/org/at/web/network/path/OvsSwitchInfo.java
+++ AT/src/org/at/web/network/path/OvsSwitchInfo.java
@@ -1,51 +1,67 @@
 package org.at.web.network.path;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.at.network.types.OvsSwitch;
 
 public class OvsSwitchInfo {
-	private List<Integer> outputPorts;
+	private HashMap<Integer, Integer> outputPorts;
 	public OvsSwitch sw;
 	private int counter;
-	
+
 	public OvsSwitchInfo(OvsSwitch sw){
 		this.sw = sw;
-		outputPorts = new ArrayList<Integer>();
+		outputPorts = new HashMap<Integer, Integer>();
 		counter = 0;
 	}
-	
+
 	public synchronized int getCounter(){
 		return counter;
 	}
-	
+
 	public synchronized void incrementCounter(){
 		counter++;
 	}
-	
+
 	public synchronized void decrementCounter(){
 		counter--;
 	}
-	
+
 	public synchronized void addOutputPort(Integer p){
-		if(!outputPorts.contains(p))
-			outputPorts.add(p);
+		Integer count = outputPorts.get(p);
+		if(count == null){
+			outputPorts.put(p, new Integer(0));
+			count = outputPorts.get(p);
+		}
+		
+		outputPorts.put(p,count+1);
 	}
-	
+
 	public synchronized void removeOutputPort(Integer p){
-		outputPorts.remove(p);
+		Integer count = outputPorts.get(p);
+		if(count != null){
+			count--;
+			if(count == 0)
+				outputPorts.remove(p);
+			else
+				outputPorts.put(p, count);
+		}
 	}
-	
-	public String getCurrentOutputActionString(){
+
+	public synchronized String getCurrentOutputActionString(){
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<outputPorts.size()-1;i++)
-			sb.append("output="+outputPorts.get(i)+",");
+		Iterator<Integer> keys = outputPorts.keySet().iterator();
+		int i = 0;
+		int size = outputPorts.keySet().size();
 		
-		sb.append("output="+outputPorts.get(outputPorts.size()-1));
-		
+		while( i < size-1 ){
+			sb.append("output="+keys.next()+",");
+			i++;
+		}
+		sb.append("output="+keys.next());
+
 		return sb.toString();
 	}
-	
-	
+
 }
