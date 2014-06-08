0d12203a720a4df480e1f8e1f1708c0281e28fd2
enrydemaio <enr.demaio@studenti.unina.it>
2014-06-08 17:34:43 +0200

    Testing PotateRelays

--- AT/src/org/at/network/NetworkConverter.java
+++ AT/src/org/at/network/NetworkConverter.java
@@ -201,29 +201,200 @@ public final class NetworkConverter {
 	
 	private static void potateRelays(VPMGraph<OvsSwitch, LinkConnection> g){
 		for(OvsSwitch sw : g.vertexSet()){
+			int k=0;
+			int j=0;
+			int i=0;
 			if(sw.type == OvsSwitch.Type.RELAY || sw.type == OvsSwitch.Type.NULL){
-				boolean hasLeaf = false;
-				boolean hasRootRelay = false;
-				
-				Set<LinkConnection> links = g.edgesOf(sw);
-				for(LinkConnection l : links){
-					if(l.isTree){
-						if( (l.getTarget().type == OvsSwitch.Type.LEAF) || l.getSource().type == OvsSwitch.Type.LEAF )
-							hasLeaf = true;
-						else if( (l.getTarget().type == OvsSwitch.Type.RELAY || l.getTarget().type == OvsSwitch.Type.ROOT)
-								|| (l.getSource().type == OvsSwitch.Type.RELAY || l.getSource().type == OvsSwitch.Type.ROOT))
-							hasRootRelay = true;
+					ArrayList<Integer> hasLeaf = new ArrayList<Integer>();
+					ArrayList<Integer> hasRoot = new ArrayList<Integer>();
+					ArrayList<String> hasRelay = new ArrayList<String>();
+					Set<LinkConnection> links = g.edgesOf(sw);
+					//Controllo che la dimensione sia maggiore di 1 in tal caso significa che non sono isolato
+					// un relay deve avere per forza due link uno verso la radice e uno vero la foglia nel caso base.
+					if (links.size() > 1){
+					
+						for(LinkConnection l : links){
+							if(l.isTree){
+								if( (l.getTarget().type == OvsSwitch.Type.LEAF) || (l.getSource().type == OvsSwitch.Type.LEAF)){
+									hasLeaf.add(1);
+									k++;
+									}
+								else if((l.getTarget().type == OvsSwitch.Type.ROOT || l.getSource().type == OvsSwitch.Type.ROOT)){
+									hasRoot.add(2);
+									j++;
+								}
+								else if (l.getTarget().type == OvsSwitch.Type.RELAY && l.getSource().type == OvsSwitch.Type.RELAY){
+									if(l.getTarget().dpid != sw.dpid){
+										//Devo ottenere in qualche modo l'OvsSwitch target
+										//OvsSwitch newTarget = l.getTarget();
+										hasRelay.add(checkNode(l.getTarget(),l,g));
+									  }
+									else {
+									   //Ottengo il prossimo relay dalla sorgente del link 
+									   hasRelay.add(checkNode(l.getSource(),l,g));
+									}
+									i++;
+								}
+							}
+						}
+						//Blocco di controlli...
+						if (k > 0) { // ho sicuramente una foglia collegata controllo se ho anche una radice
+							if (!(j>0)){ // non ho una radice controllo se ho qualche relay
+								if(i>0){ // ho i relay
+									// controllo se qualcuno e' attaccato alla radice altrimenti cancello i miei link
+									int n=0; 
+									boolean isRooted=false;
+									while(n<i){
+										if(hasRelay.get(n).contains("ROOT")){
+											isRooted=true;
+											break;
+										}
+										n++;
+									}
+									if (!isRooted){
+										for(LinkConnection l : g.edgesOf(sw)){
+										if(l.isTree)
+											l.isTree = false;
+										}
+									}
+									
+								}else { //non ho i relay quindi sono solo attaccato alla foglia posso eliminare
+									for(LinkConnection l : g.edgesOf(sw)){
+										if(l.isTree)
+											l.isTree = false;
+									}
+								}
+							}
+						}
+						else {
+							if (!(j>0)){ // non ho una radice e nemmeno una foglia controllo se ho qualche relay
+								if(i>0){ // ho i relay
+									// controllo se qualcuno e' attaccato alla radice e ad una foglia altrimenti cancello i miei link
+									int n=0; 
+									boolean isRooted=false;
+									boolean hasALeaf=false;
+									while(n<i){
+										if(hasRelay.get(n).contains("ROOT") ){
+											isRooted=true;
+											
+										}
+										if(hasRelay.get(n).contains("LEAF")){
+										   hasALeaf=true;
+										}
+										n++;
+									}
+									if (!isRooted && !hasALeaf){ //non hanno radice ne foglie e una catena di relay
+										for(LinkConnection l : g.edgesOf(sw)){
+										if(l.isTree)
+											l.isTree = false;
+										}
+									}
+									
+								}else { //non ho i relay quindi sono solo attaccato alla foglia posso eliminare
+									for(LinkConnection l : g.edgesOf(sw)){
+										if(l.isTree)
+											l.isTree = false;
+									}
+								}
+							}
+							else {
+								//sono attaccato ad una radice ma non ho foglie controllo se ho relay che hanno foglie
+								if(i>0){ // ho i relay
+									// controllo se qualcuno e' attaccato alla radice e ad una foglia altrimenti cancello i miei link
+									int n=0; 
+									boolean hasALeaf=false;
+									while(n<i){
+										if(hasRelay.get(n).contains("LEAF")){
+										   hasALeaf=true;
+										   break;
+										}
+										n++;
+									}
+									if (!hasALeaf){ //non hanno foglie e una catena di relay
+										for(LinkConnection l : g.edgesOf(sw)){
+										if(l.isTree)
+											l.isTree = false;
+										}
+									}
+									
+								}else { //non ho i relay quindi sono solo attaccato alla foglia posso eliminare
+									for(LinkConnection l : g.edgesOf(sw)){
+										if(l.isTree)
+											l.isTree = false;
+									}
+								}
+							}
+						}
+						
+						
+						
 					}
+					else {
+						for(LinkConnection l : g.edgesOf(sw))
+							if(l.isTree)
+								l.isTree = false;
+					}
+
+					
 				}
-				
-				if(!(hasLeaf && hasRootRelay) ){
-					for(LinkConnection l : g.edgesOf(sw))
-						if(l.isTree)
-							l.isTree = false;
-				}
-			}
 		}
+		
 	}
+ 
+ private static String checkNode(OvsSwitch sw, LinkConnection link, VPMGraph<OvsSwitch,LinkConnection> g){
+	String ret = "";
+	int root=0;
+	int leaf=0;
+	int count = 0; // se count e uno significa che il relay e isolato e non dobbiamo considerarlo nell'albero
+	Set<LinkConnection> links = g.edgesOf(sw);
+	for(LinkConnection l : links){
+		if(l.isTree && !l.equals(link)){
+			if( (l.getTarget().type == OvsSwitch.Type.RELAY) && (l.getSource().type == OvsSwitch.Type.RELAY)){
+			  if(l.getTarget().dpid != sw.dpid){
+				//Devo ottenere in qualche modo i,l relay dal target del link l
+				ret=checkNode(l.getTarget(),l,g);
+			  }
+			  else {
+			   //Ottengo il prossimo relay dalla sorgente del link 
+			   
+			   ret=checkNode(l.getSource(),l,g);
+			  }
+			}
+			else if( (l.getTarget().type == OvsSwitch.Type.LEAF && l.getSource().type == OvsSwitch.Type.RELAY) || 
+			(l.getTarget().type == OvsSwitch.Type.RELAY && l.getSource().type == OvsSwitch.Type.LEAF)){ //relay connesso ad una foglia
+					leaf+=1; //ritorno 1 se direttamente attaccato ad una leaf; 
+			}
+			else if( (l.getTarget().type == OvsSwitch.Type.RELAY && l.getSource().type == OvsSwitch.Type.ROOT) || 
+			(l.getTarget().type == OvsSwitch.Type.ROOT && l.getSource().type == OvsSwitch.Type.RELAY)){ //relay connesso direttamente alla root
+				root+=1;
+			}
+			count++;
+		}
+		else if(l.isTree && l.equals(link)) count++;
+	}
+	if (count > 1){
+		if(leaf > 0) {
+			if(root > 0){
+				if (ret != ""){
+					 if(!ret.contains("LEAF")) 
+						ret+="LEAF"+","+"ROOT,";
+					 else
+						ret+="ROOT,"; //se gia vie e una foglia inserisco solo la radice nel valore di ritorno
+				}
+				else ret+="LEAF"+","+"ROOT,";
+				
+			}
+			else if (ret != ""){
+				if(!ret.contains("LEAF")) //se nel valore di ritorno non vi e gia una foglia la inserisco
+					ret+="LEAF,";
+			}
+			else ret+="LEAF";
+		}
+		else if (root>0) ret+="ROOT,"; //ci sara solo una root
+		return ret;
+	}
+	else return "";
+ }
 	
 	//****************************************************************************************
 	
@@ -234,12 +405,12 @@ public final class NetworkConverter {
 		
 		OvsSwitch a = new OvsSwitch("a","1",OvsSwitch.Type.ROOT);
 		OvsSwitch b = new OvsSwitch("b","2", OvsSwitch.Type.RELAY);
-		OvsSwitch c = new OvsSwitch("c","3", OvsSwitch.Type.LEAF);
+		OvsSwitch c = new OvsSwitch("c","3", OvsSwitch.Type.RELAY);
 		OvsSwitch d = new OvsSwitch("d","4", OvsSwitch.Type.RELAY);
 		OvsSwitch e = new OvsSwitch("e","5",OvsSwitch.Type.RELAY);
-		OvsSwitch f = new OvsSwitch("f","6",OvsSwitch.Type.RELAY);
+		OvsSwitch f = new OvsSwitch("f","6",OvsSwitch.Type.LEAF);
 		OvsSwitch g = new OvsSwitch("g","7",OvsSwitch.Type.RELAY);
-		OvsSwitch h = new OvsSwitch("h","8",OvsSwitch.Type.RELAY);
+		OvsSwitch h = new OvsSwitch("h","8",OvsSwitch.Type.LEAF);
 		
 		myGraph.addVertex(a);
 		myGraph.addVertex(b);
@@ -263,10 +434,10 @@ public final class NetworkConverter {
 		
 		VPMGraph<OvsSwitch, LinkConnection> myGraph2 = new VPMGraph<>(LinkConnection.class);
 		
-		OvsSwitch copy = new OvsSwitch("a","1");
-		OvsSwitch copy2 = new OvsSwitch("b","2", OvsSwitch.Type.RELAY);
+		//OvsSwitch copy = new OvsSwitch("a","1");
+		//OvsSwitch copy2 = new OvsSwitch("b","2", OvsSwitch.Type.RELAY);
 		
-		myGraph2.addVertex(copy);
+		myGraph2.addVertex(a);
 		myGraph2.addVertex(b);
 		myGraph2.addVertex(c);
 		myGraph2.addVertex(d);
@@ -276,7 +447,7 @@ public final class NetworkConverter {
 		myGraph2.addVertex(h);
 		
 		LinkConnection[] conns2 = new LinkConnection[8];
-	    conns2[0] = myGraph2.addLinkConnection(copy, new Port("p1",1),copy2,new Port("p2",2));
+	    conns2[0] = myGraph2.addLinkConnection(a, new Port("p1",1),b,new Port("p2",2));
 		conns2[1] = myGraph2.addLinkConnection(c, new Port("p2",2),b,new Port("p3",1));
 		conns2[2] = myGraph2.addLinkConnection(b, new Port("p3",1),d,new Port("p4",2));
 		conns2[3] = myGraph2.addLinkConnection(d, new Port("p1",4),e,new Port("p2",1));
@@ -285,11 +456,6 @@ public final class NetworkConverter {
 	    conns2[6] = myGraph2.addLinkConnection(d, new Port("p6",7),g,new Port("p3",1));
 		conns2[7] = myGraph2.addLinkConnection(g, new Port("p6",7),h,new Port("p3",1));		
 		
-
-		
-		
-		//myGraph2.equals(myGraph);
-		System.out.print(myGraph2.containsEdge(b, c));
 		/*OvsSwitch b1 = new OvsSwitch("b","2");
 		OvsSwitch d1 = new OvsSwitch("d","4");
 		
@@ -302,28 +468,12 @@ public final class NetworkConverter {
 		
 		
 		
-		/*createTree(myGraph);
+		createTree(myGraph);
 		potateRelays(myGraph);
 		for(LinkConnection l : myGraph.edgeSet()){
 			if(l.isTree)
 				System.out.println(l);
 		}
-		
-		
-		System.out.println("Is connected: "+cIsp.isGraphConnected());
-		myGraph.removeEdge(conns[5]);
-		myGraph.removeEdge(conns[4]);*/
-		
-		//cIsp = new ConnectivityInspector<>(myGraph);
-		//System.out.println("Is connected: "+cIsp.isGraphConnected());
-		/*DijkstraShortestPath<OvsSwitch, LinkConnection> dj = new DijkstraShortestPath<OvsSwitch, LinkConnection>(myGraph, 
-				b1, d1);
-		
-		
-		mxGraph dx = NetworkConverter.jpathToMx(dj.getPath());*/
-		//mxCodec codec = new mxCodec();	
-		//System.out.println(mxUtils.getPrettyXml(codec.encode(dx.getModel())));//
-		
 		
 		
 		
